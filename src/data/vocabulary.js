b73a6afec1d91c9e148ada387869c63fadf67cf6
Xavier Jean <axrevi@gmail.com>
2022-04-08 15:32:11 +0000

    Replace vocabulary.js

--- src/data/vocabulary.js
+++ src/data/vocabulary.js
@@ -17,6 +17,108 @@ const vocabulary = [
         'translation': 'fête des mères',
     },
     {
+      'elements': [
+          {
+              'kanji': '日',
+              'kana': 'ニチ',
+          },
+          {
+              'kanji': '曜',
+              'kana': 'ヨウ',
+          },
+          {
+              'kanji': '日',
+              'kana': 'び',
+          },
+      ],
+      'translation': 'dimanche',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '月',
+              'kana': 'ゲツ',
+          },
+          {
+              'kanji': '曜',
+              'kana': 'ヨウ',
+          },
+          {
+              'kanji': '日',
+              'kana': 'び',
+          },
+      ],
+      'translation': 'lundi',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '火',
+              'kana': 'カ',
+          },
+          {
+              'kanji': '曜',
+              'kana': 'ヨウ',
+          },
+          {
+              'kanji': '日',
+              'kana': 'び',
+          },
+      ],
+      'translation': 'mardi',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '水',
+              'kana': 'スイ',
+          },
+          {
+              'kanji': '曜',
+              'kana': 'ヨウ',
+          },
+          {
+              'kanji': '日',
+              'kana': 'び',
+          },
+      ],
+      'translation': 'mercredi',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '木',
+              'kana': 'モク',
+          },
+          {
+              'kanji': '曜',
+              'kana': 'ヨウ',
+          },
+          {
+              'kanji': '日',
+              'kana': 'び',
+          },
+      ],
+      'translation': 'jeudi',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '金',
+              'kana': 'キン',
+          },
+          {
+              'kanji': '曜',
+              'kana': 'ヨウ',
+          },
+          {
+              'kanji': '日',
+              'kana': 'び',
+          },
+      ],
+      'translation': 'vendredi',
+    },
+    {
         'elements': [
             {
                 'kanji': '土',
@@ -37,23 +139,6 @@ const vocabulary = [
         'elements': [
             {
                 'kanji': '日',
-                'kana': 'ニチ',
-            },
-            {
-                'kanji': '曜',
-                'kana': 'ヨウ',
-            },
-            {
-                'kanji': '日',
-                'kana': 'び',
-            },
-        ],
-        'translation': 'dimanche',
-    },
-    {
-        'elements': [
-            {
-                'kanji': '日',
                 'kana': '二',
             },
             {
@@ -62,6 +147,368 @@ const vocabulary = [
             },
         ],
         'translation': 'Japon',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '食',
+              'kana': 'た',
+          },
+          {
+              'kanji': '',
+              'kana': 'べ',
+          },
+          {
+            'kanji': '',
+            'kana': 'る',
+          },
+      ],
+      'translation': 'manger',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '食',
+              'kana': 'た',
+          },
+          {
+              'kanji': '',
+              'kana': 'べ',
+          },
+          {
+            'kanji': '物',
+            'kana': 'もの',
+          },
+      ],
+      'translation': 'nourriture',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '飲',
+              'kana': 'の',
+          },
+          {
+              'kanji': '',
+              'kana': 'む',
+          },
+      ],
+      'translation': 'boire',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '飲',
+              'kana': 'の',
+          },
+          {
+              'kanji': '',
+              'kana': 'み',
+          },
+          {
+            'kanji': '物',
+            'kana': 'もの',
+          },
+      ],
+      'translation': 'boisson',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '薬',
+              'kana': 'くすり',
+          },
+          {
+              'kanji': '屋',
+              'kana': 'や',
+          },
+      ],
+      'translation': 'pharmacie',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '',
+              'kana': 'パン',
+          },
+          {
+              'kanji': '屋',
+              'kana': 'や',
+          },
+      ],
+      'translation': 'boulangerie',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '夕',
+              'kana': 'ゆう',
+          },
+          {
+              'kanji': '食',
+              'kana': 'ショク',
+          },
+      ],
+      'translation': 'dîner',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '火',
+              'kana': 'カ',
+          },
+          {
+              'kanji': '山',
+              'kana': 'ザン',
+          },
+      ],
+      'translation': 'volcan',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '大',
+              'kana': 'ダイ',
+          },
+          {
+              'kanji': '学',
+              'kana': 'ガク',
+          },
+      ],
+      'translation': 'université',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '学',
+              'kana': 'ガッ',
+          },
+          {
+            'kanji': '校',
+            'kana': 'コウ',
+          },
+      ],
+      'translation': 'école',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '学',
+              'kana': 'ガク',
+          },
+          {
+            'kanji': '生',
+            'kana': 'セイ',
+          },
+      ],
+      'translation': 'étudiant',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '先',
+              'kana': 'セン',
+          },
+          {
+            'kanji': '生',
+            'kana': 'セイ',
+          },
+      ],
+      'translation': 'professeur',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '花',
+              'kana': 'はな',
+          },
+          {
+            'kanji': '火',
+            'kana': 'び',
+          },
+      ],
+      'translation': 'feu d\'artifice',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '一',
+              'kana': 'イチ',
+          },
+          {
+              'kanji': '月',
+              'kana': 'ガツ',
+          },
+      ],
+      'translation': 'janvier',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '二',
+              'kana': 'に',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'février',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '三',
+              'kana': 'サン',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'mars',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '四',
+              'kana': 'シ',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'avril',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '五',
+              'kana': 'ゴ',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'mai',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '六',
+              'kana': 'ロク',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'juin',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '七',
+              'kana': 'シチ',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'juillet',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '八',
+              'kana': 'ハチ',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'août',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '九',
+              'kana': 'ク',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'septembre',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '十',
+              'kana': 'ジュウ',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'octobre',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '十',
+              'kana': 'ジュウ',
+          },
+          {
+              'kanji': '一',
+              'kana': 'イチ',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'novembre',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '十',
+              'kana': 'ジュウ',
+          },
+          {
+              'kanji': '二',
+              'kana': 'に',
+          },
+          {
+            'kanji': '月',
+            'kana': 'ガツ',
+          },
+      ],
+      'translation': 'décembre',
+    },
+    {
+      'elements': [
+          {
+              'kanji': '分',
+              'kana': 'わ',
+          },
+          {
+              'kanji': '',
+              'kana': 'か',
+          },
+          {
+            'kanji': '',
+            'kana': 'る',
+          },
+      ],
+      'translation': 'comprendre',
     },
 ];
 
