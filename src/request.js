2330ed364974c950130b27cfbcba8ce7597b6310
Xavier Maillou <axrevi@gmail.com>
2022-10-31 00:08:47 +0100

    request url change depending on env

--- src/request.js
+++ src/request.js
@@ -1,47 +1,49 @@
 import axios from 'axios'
 import config from './config.json'
 
+const API_URL = process.env.NODE_ENV === 'development' ? config.DEV_REQUEST_URL : config.PROD_REQUEST_URL
+
 export const fetchKanjiList = async (level, grammar, collection, search, offset, setLoading) => {
     setLoading(true)
-    const result = await axios.get(`${config.REQUEST_URL}/kanjiList/${offset}/${level}/${grammar}/${collection}/${search}`)
+    const result = await axios.get(`${API_URL}/kanjiList/${offset}/${level}/${grammar}/${collection}/${search}`)
     setLoading(false)
     return result.data
 }
 
 export const fetchVocabularyList = async (level, grammar, collection, search, offset, setLoading) => {
     setLoading(true)
-    const result = await axios.get(`${config.REQUEST_URL}/vocabularyList/${offset}/${level}/${grammar}/${collection}/${search}`)
+    const result = await axios.get(`${API_URL}/vocabularyList/${offset}/${level}/${grammar}/${collection}/${search}`)
     setLoading(false)
     return result.data
 }
 
 export const fetchSentences = async (id, setLoading) => {
     setLoading(true)
-    const result = await axios.get(`${config.REQUEST_URL}/sentences/${id}`)
+    const result = await axios.get(`${API_URL}/sentences/${id}`)
     setLoading(false)
     return result.data
 }
 
 export const fetchKanji = async (id, setLoading) => {
     setLoading(true)
-    const result = await axios.get(`${config.REQUEST_URL}/kanji/${id}`)
+    const result = await axios.get(`${API_URL}/kanji/${id}`)
     setLoading(false)
     return result.data
 }
 
 export const fetchWord = async (id, setLoading) => {
     setLoading(true)
-    const result = await axios.get(`${config.REQUEST_URL}/word/${id}`)
+    const result = await axios.get(`${API_URL}/word/${id}`)
     setLoading(false)
     return result.data
 }
 
 export const fetchKanjiTraining = async (level, grammar, collection) => {
-    const result = await axios.get(`${config.REQUEST_URL}/kanjiTrainingList/${level}/${grammar}/${collection}`)
+    const result = await axios.get(`${API_URL}/kanjiTrainingList/${level}/${grammar}/${collection}`)
     return result.data
 }
 
 export const fetchVocabularyTraining = async (level, grammar, collection) => {
-    const result = await axios.get(`${config.REQUEST_URL}/vocabularyTrainingList/${level}/${grammar}/${collection}`)
+    const result = await axios.get(`${API_URL}/vocabularyTrainingList/${level}/${grammar}/${collection}`)
     return result.data
 }
