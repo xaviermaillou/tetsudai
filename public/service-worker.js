839af2c89116b0683830ef80978873f798a99d4e
Xavier Maillou <axrevi@gmail.com>
2024-06-07 14:25:44 +0200

    service worker fix + CSS fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,20 @@
 const CACHE_NAME = 'tetsudai-cache'
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request)
-          .then(async (response) => {
-            const cache = await caches.open(CACHE_NAME)
-            cache.put(event.request, response.clone())
-            return response
-          })
+  event.respondWith(
+    fetch(event.request)
+    .then(async (response) => {
+        if (event.request.method === "POST") return
+        const cache = await caches.open(CACHE_NAME)
+        cache.put(event.request, response.clone())
+        return response
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then((cachedResponse) => cachedResponse || fetch(event.request))
           .catch(() => {
-            return caches.match(event.request)
-              .then((cachedResponse) => cachedResponse || fetch(event.request))
-              .catch(() => {
-                return new Response(null)
-              })
+            return new Response(null)
           })
-      )
+      })
+  )
 })
