f52836a0c23e9886602594f8b6ed958a47cd47e1
Xavier Maillou <axrevi@gmail.com>
2024-10-26 23:56:03 +0200

    service worker improvement

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,17 +4,18 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request)
     .then(async (response) => {
-        if (event.request.method === "POST") return response
-        const cache = await caches.open(CACHE_NAME)
-        cache.put(event.request, response.clone())
-        return response
-      })
-      .catch(() => {
-        return caches.match(event.request)
-          .then((cachedResponse) => cachedResponse || fetch(event.request))
-          .catch(() => {
-            return new Response(null)
-          })
-      })
+      if (event.request.method === "POST") return response
+      if (new URL(event.request.url).pathname.toLowerCase().includes("list")) return response
+      const cache = await caches.open(CACHE_NAME)
+      cache.put(event.request, response.clone())
+      return response
+    })
+    .catch(async () => {
+      return caches.match(event.request)
+        .then((cachedResponse) => cachedResponse)
+        .catch(() => {
+          return new Response(null)
+        })
+    })
   )
 })
